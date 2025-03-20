3051b56a852914b905474cbb3cb052a2b54b9953
Kevin PESET <kevin.peset@gmail.com>
2025-03-20 18:06:00 +0100

    Ajout des mouvement du player + créations de plateformes

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,21 +1,37 @@
-import { type Scene, Physics } from "phaser";
+import { type Scene, Physics, type Types } from "phaser";
 
 export class Player extends Physics.Arcade.Sprite {
-	// do something
+	cursors: Types.Input.Keyboard.CursorKeys;
+
 	constructor(scene: Scene, x: number, y: number) {
-		// Charger le sprite du joueur
 		super(scene, x, y, "dude");
 
-		// Ajoute le joueur et active la physique
 		scene.add.existing(this);
 		scene.physics.add.existing(this);
 
 		const bounce = 0.2;
 		const gravity = 300;
 
-		// Configuration du joueur
 		this.setBounce(bounce);
 		this.setCollideWorldBounds(true);
 		this.setGravityY(gravity);
+
+		if (scene.input.keyboard) {
+			this.cursors = scene.input.keyboard.createCursorKeys();
+		}
+	}
+
+	move() {
+		if (this.cursors.left.isDown) {
+			this.setVelocityX(-160);
+		} else if (this.cursors.right.isDown) {
+			this.setVelocityX(160);
+		} else {
+			this.setVelocityX(0);
+		}
+
+		if (this.cursors.up.isDown && this.body?.touching.down) {
+			this.setVelocityY(-330);
+		}
 	}
 }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -8,22 +8,10 @@ export class Game extends Scene {
 	}
 
 	platforms: Platforms;
-	player: Phaser.Physics.Arcade.Sprite;
+	player: Player;
 
 	create() {
-		// Affichage de l'image sky à l'écran.
-		// L'affichage se fait dans l'ordre.
-
 		this.add.image(400, 300, "sky");
-
-		// crée un groupe statique d'objets qui ne bougent pas et qui peuvent être utilisés pour gérer des
-		// collisions dans le jeu.
-
-		// Affichage des platformes
-
-		// Nous aggrandissons la première plateforme pour lui donner une impression de hauteur.
-		// Sans  la méthode refreshBody(), la boîte de collision resterait à la taille originale de l'image,
-		// ce qui peut causer des bugs de collision où le joueur "tombe à travers" la plateforme.
 
 		this.player = new Player(this, 100, 450);
 		this.platforms = new Platforms(this);
@@ -31,5 +19,15 @@ export class Game extends Scene {
 		this.physics.add.collider(this.player, this.platforms);
 
 		this.platforms.createPlatform(400, 600 - 32, "ground", 2);
+		this.platforms.createPlatform(400, 500 - 32, "ground");
+		this.platforms.createPlatform(100, 420 - 32, "ground");
+		this.platforms.createPlatform(550, 350 - 32, "ground");
+		this.platforms.createPlatform(350, 270 - 32, "ground");
+		this.platforms.createPlatform(100, 200 - 32, "ground");
+		this.platforms.createPlatform(640, 170 - 32, "ground");
+	}
+
+	update(): void {
+		this.player.move();
 	}
 }
