64525bac25e82688bac1cb79e9cb5545f0739ae0
Kevin PESET <kevin.peset@gmail.com>
2025-03-21 10:36:36 +0100

    Ajout animations lors du déplacement du joueur

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -16,6 +16,26 @@ export class Player extends Physics.Arcade.Sprite {
 		this.setCollideWorldBounds(true);
 		this.setGravityY(gravity);
 
+		this.anims.create({
+			key: "left",
+			frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1,
+		});
+
+		this.anims.create({
+			key: "turn",
+			frames: [{ key: "dude", frame: 4 }],
+			frameRate: 20,
+		});
+
+		this.anims.create({
+			key: "right",
+			frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+			frameRate: 10,
+			repeat: -1,
+		});
+
 		if (scene.input.keyboard) {
 			this.cursors = scene.input.keyboard.createCursorKeys();
 		}
@@ -24,10 +44,13 @@ export class Player extends Physics.Arcade.Sprite {
 	move() {
 		if (this.cursors.left.isDown) {
 			this.setVelocityX(-160);
+			this.anims.play("left", true);
 		} else if (this.cursors.right.isDown) {
 			this.setVelocityX(160);
+			this.anims.play("right", true);
 		} else {
 			this.setVelocityX(0);
+			this.anims.play("turn", true);
 		}
 
 		if (this.cursors.up.isDown && this.body?.touching.down) {
